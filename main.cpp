90b6c4524b164b21ecda2b3c9f2fdc5a302cf798
neucoder <neucoder@163.com>
2019-05-15 21:42:19 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    string s = "123";
+    cout << s << endl;
+    cout << "Hello, World!" << endl;
+    return 0;
+}
